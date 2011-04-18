22e220e05d7f09ee0658c7682ab2e7899aba4f77
Takeshi Watanabe <takechi101010@gmail.com>
2011-04-19 01:15:34 +0900

    added pthread-emb

---
+++ scripts/028-pthreads-emb-1.0.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# pthreads-emb-1.0.sh by take_cheeze (takechi101010@gmail.com)
+
+## Download
+wget --continue http://jaist.dl.sourceforge.net/project/pthreads-emb/pthreads-emb/1.0/pthreads-emb-1.0.tar.gz
+
+
+## Unpack
+rm -Rf pthreads-emb-1.0 && mkdir pthreads-emb-1.0 && tar --strip-components=1 --directory=pthreads-emb-1.0 -xvzf pthreads-emb-1.0.tar.gz || { exit 1; }
+
+## Enter
+cd pthreads-emb-1.0 || { exit 1; }
+
+## Compile and Install
+make -C platform/psp -j 4 && make -C platform/psp install || { exit 1; }
+cp -v *.h `psp-config --psp-prefix`/include
