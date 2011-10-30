2c334607af66a71a5fa37a5dd112454562bfc3a7
take_cheeze <takechi101010@gmail.com>
2011-10-30 19:28:48 +0900

    use ccache and enable c++0x

--- scripts/034-boost.sh
+++ scripts/034-boost.sh
@@ -14,7 +14,7 @@ rm -Rf $DIRECTORY && mkdir $DIRECTORY && tar --strip-components=1 --directory=$D
 cd $DIRECTORY || { exit 1; }
 
 ## Patch
-echo "using gcc : 4.5.3 : $(psp-config --pspdev-path)/bin/psp-g++ : <cxxflags>-I$(psp-config --pspsdk-path)/include ;" \
+echo "using gcc : 4.5.3 : ccache $(psp-config --pspdev-path)/bin/psp-g++ -std=c++0x : <cxxflags>-I$(psp-config --pspsdk-path)/include ;" \
 	> tools/build/v2/user-config.jam  \
 || { exit 1; }
 
