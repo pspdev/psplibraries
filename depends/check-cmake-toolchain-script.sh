d5cd2b6b8702c7da658e3bc928fccb55312cc5eb
take_cheeze <takechi101010@gmail.com>
2012-03-04 01:49:23 +0900

    improve dependency script

--- depends/check-cmake-toolchain-script.sh
+++ depends/check-cmake-toolchain-script.sh
@@ -2,5 +2,6 @@
 # check-cmake-toolchain-script.sh by Takeshi Watanabe (takechi101010@gmail.com)
 
  ## Check for cmake toolchain script.
- $(psp-config --pspdev-path)/bin/psp-cmake
- $(psp-config --psp-prefix)/share/cmake-2.8/Modules/Platform
+ ls \
+     $(psp-config --pspdev-path)/bin/psp-cmake \
+     $(psp-config --psp-prefix)/share/cmake-2.8/Modules/Platform/PSP.cmake
