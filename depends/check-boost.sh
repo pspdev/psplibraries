546b749cf540c6176f9bd45197f81249386fda1b
take_cheeze <takechi101010@gmail.com>
2012-03-04 21:18:49 +0900

    remove boost

--- depends/check-boost.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
- ls $(psp-config --psp-prefix)/include/boost/config.hpp
- ls $(psp-config --psp-prefix)/include/boost/utility/enable_if.hpp
- ls $(psp-config --psp-prefix)/include/boost/type_traits.hpp
