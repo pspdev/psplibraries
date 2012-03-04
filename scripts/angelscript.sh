82b6ff6b410e13e9724b6369a5882aa8fead0b67
take_cheeze <takechi101010@gmail.com>
2012-03-04 21:17:40 +0900

    cleaning

--- scripts/angelscript.sh
+++ scripts/angelscript.sh
@@ -5,10 +5,8 @@ ANGELSCRIPT_VERSION=2.22.2
 
 download_and_extract http://www.angelcode.com/angelscript/sdk/files/angelscript_$ANGELSCRIPT_VERSION.zip sdk
 cd angelscript/projects/cmake || { return 1; }
-echo "install(DIRECTORY ../../include DESTINATION include FILES_MATCHING PATTERN '*.h')" \
->> CMakelists.txt
-echo "install(TARGETS Angelscript ARCHIVE DESTINATION lib)" \
->> CMakelists.txt
+echo "install(DIRECTORY ../../include DESTINATION include FILES_MATCHING PATTERN '*.h')" >> CMakelists.txt
+echo "install(TARGETS Angelscript ARCHIVE DESTINATION lib)" >> CMakelists.txt
 
 ## Run cmake
 psp-cmake -D CMAKE_INSTALL_PREFIX=$(psp-config --psp-prefix) . || { exit 1; }
