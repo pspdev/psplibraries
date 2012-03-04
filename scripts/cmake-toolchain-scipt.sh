a51f0021f48054cd6e59a35cc5e9987487ea9a5b
take_cheeze <takechi101010@gmail.com>
2012-03-04 22:04:03 +0900

    mkdir for toolchain script

--- scripts/cmake-toolchain-scipt.sh
+++ scripts/cmake-toolchain-scipt.sh
@@ -7,5 +7,6 @@ PSP_CMAKE_PATH=$(psp-config --pspdev-path)/bin/psp-cmake
 TOOLCHAIN_SCRIPT_PATH=$(psp-config --psp-prefix)/share/cmake-2.8/Modules/Platform
 
 ## copy toolchain script and psp-cmake
+mkdir -p $TOOLCHAIN_SCRIPT_PATH
 cp ../patches/PSP.cmake $TOOLCHAIN_SCRIPT_PATH || { exit 1; }
 cp ../patches/psp-cmake $PSP_CMAKE_PATH || { exit 1; }
