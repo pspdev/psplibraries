19def5244adf28defcab895b5aa948c51f8cd07c
take_cheeze <takechi101010@gmail.com>
2012-03-05 12:35:33 +0900

    fix angelscript install

--- scripts/angelscript.sh
+++ scripts/angelscript.sh
@@ -5,11 +5,11 @@ ANGELSCRIPT_VERSION=2.22.2
 
 download_and_extract http://www.angelcode.com/angelscript/sdk/files/angelscript_$ANGELSCRIPT_VERSION.zip sdk
 cd angelscript/projects/cmake || { return 1; }
-echo "install(DIRECTORY ../../include DESTINATION include FILES_MATCHING PATTERN \"*.h\")" >> CMakeLists.txt
-echo "install(TARGETS Angelscript ARCHIVE DESTINATION lib)" >> CMakeLists.txt
 
 ## Run cmake
 psp-cmake -D CMAKE_INSTALL_PREFIX=$(psp-config --psp-prefix) . || { exit 1; }
 
 ## Compile and Install
-make -j 4 && make install || { exit 1; }
+make -j 4 || { exit 1; }
+cp -v ../../include/*.h $(psp-config --psp-prefix)/include
+cp -v ../../lib/lib*.a $(psp-config --psp-prefix)/lib
