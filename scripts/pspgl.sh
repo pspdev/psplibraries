ed408d377f4317be6d578367d057eeacede53d58
take_cheeze <takechi101010@gmail.com>
2012-03-05 13:08:01 +0900

    use tmp file instead of sed

--- scripts/pspgl.sh
+++ scripts/pspgl.sh
@@ -1,8 +1,10 @@
 get_pspports pspgl
 
-for i in glLockArraysEXT pspgl_dlist pspgl_vidmem;
-do sed '1i \
-#include <psptypes.h>' $i.c > $i.c;
+for i in glLockArraysEXT pspgl_dlist pspgl_vidmem
+do
+echo '#include <psptypes.h>' > $i.c.tmp
+cat $i.c >> $i.c.tmp
+cat $i.c.tmp > $i.c
 done
 
 run_make -j 4
