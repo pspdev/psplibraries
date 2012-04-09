832162f4a3e92d57da67c0740ab519e86d55c6fa
take_cheeze <takechi101010@gmail.com>
2012-04-10 00:21:29 +0900

    add dependencies for some libraries

--- scripts/libpng.sh
+++ scripts/libpng.sh
@@ -1,3 +1,5 @@
+test_deps_install zlib
+
 get_pspports libpng
 run_make -j 4
 
--- scripts/opentri.sh
+++ scripts/opentri.sh
@@ -1,3 +1,5 @@
+test_deps_install zlib freetype
+
 wget --continue --no-check-certificate https://github.com/SamRH/openTRI/tarball/master -O openTRI.tar.gz
 rm -Rf openTRI && mkdir openTRI && tar --strip-components=1 --directory=openTRI -xvzf openTRI.tar.gz
 cd openTRI
