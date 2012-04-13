8383d0db59b3e02c67ebd146cf3f3471b4c4b9a7
take_cheeze <takechi101010@gmail.com>
2012-04-14 01:25:42 +0900

    fix smpeg-psp compile error and add check-smpeg-psp.sh

--- scripts/smpeg-psp.sh
+++ scripts/smpeg-psp.sh
@@ -4,5 +4,6 @@ else
   svn update smpeg-psp
 fi
 cd smpeg-psp
+sed -i -e "s/static __inline__ Uint16 SDL_Swap16(Uint16 x)/static __inline__ Uint16 Disable_SDL_Swap16(Uint16 x)/" audio/*.cpp || { exit 1; }
 run_make -j 4
 
