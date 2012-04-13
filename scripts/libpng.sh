60f22c511dcca58ab46e2091c390f284b17a2a92
take_cheeze <takechi101010@gmail.com>
2012-04-14 04:55:06 +0900

    fix for pixman

--- scripts/libpng.sh
+++ scripts/libpng.sh
@@ -3,3 +3,4 @@ test_deps_install zlib
 get_pspports libpng
 run_make -j 4
 
+ln -s $(psp-config --psp-prefix)/lib/libpng.a $(psp-config --psp-prefix)/lib/libpng12.a
--- scripts/pixman.sh
+++ scripts/pixman.sh
@@ -2,6 +2,6 @@ PIXMAN_VERSION=0.24.4
 
 download_and_extract http://cairographics.org/releases/pixman-$PIXMAN_VERSION.tar.gz pixman-$PIXMAN_VERSION
 cp ../../patches/config.sub ./config.sub
-run_configure
+LIBS="-lz" run_configure
 run_make -j 4
 
