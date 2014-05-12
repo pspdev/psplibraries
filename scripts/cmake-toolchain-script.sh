63c0bf0829c7eb12075c7760ca6bb07cbdf1af6c
xantares <xantares09@hotmail.com>
2014-05-12 18:52:44 +0200

    Ok, removed DESTDIR.

--- scripts/cmake-toolchain-script.sh
+++ scripts/cmake-toolchain-script.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # cmake-toolchain-script.sh by take_cheeze (takechi101010@gmail.com)
 DESTDIR=$1
-PSP_CMAKE_PATH=${DESTDIR}$(psp-config --pspdev-path)/bin
-TOOLCHAIN_SCRIPT_PATH=${DESTDIR}$(psp-config --psp-prefix)/share/cmake-2.8/Modules/Platform
+PSP_CMAKE_PATH=$(psp-config --pspdev-path)/bin
+TOOLCHAIN_SCRIPT_PATH=$(psp-config --psp-prefix)/share/cmake-2.8/Modules/Platform
 
 ## copy toolchain script and psp-cmake
 install -d $TOOLCHAIN_SCRIPT_PATH
