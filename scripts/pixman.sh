fd57979961a6042b9735fea657802bef5384b985
Johannes Ekberg <uppfinnarn@gmail.com>
2014-06-08 15:16:08 +0200

    Fixed pixman

--- scripts/pixman.sh
+++ scripts/pixman.sh
@@ -2,6 +2,6 @@ PIXMAN_VERSION=0.24.4
 
 download_and_extract http://cairographics.org/releases/pixman-$PIXMAN_VERSION.tar.gz pixman-$PIXMAN_VERSION
 cp ../../patches/config.sub ./config.sub
-LIBS="-lz" run_configure
+PNG_CFLAGS="-I/usr/local/pspdev/psp/include" PNG_LIBS="-L/usr/local/pspdev/psp/lib -lpng -lz" run_configure
 run_make -j 4
 
