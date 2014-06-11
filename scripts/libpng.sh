dc0ec153ee7989cff6e34d737f228094cff53b5c
Bracket- <Bracket-@users.noreply.github.com>
2014-06-11 18:19:57 -0400

    libpng: Use run_autogen_build instead of manually configuring
    I have a feeling that although the library built, it was probably broken
    (1) Used autogen instead of configure right after running autogen
    (2) Missing assignment operator '=' for --host

--- scripts/libpng.sh
+++ scripts/libpng.sh
@@ -1,5 +1,4 @@
 test_deps_install zlib
 
 get_pspports libpng
-chmod a+x ./autogen.sh && LDFLAGS="-L$(psp-config --pspsdk-path)/lib" LIBS="-lc -lpspuser" ./autogen.sh --host psp --prefix=$(psp-config --psp-prefix) || { exit 1; }
-run_make -j 4
+run_autogen_build
