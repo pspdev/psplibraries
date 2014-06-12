7ff883aa1cf1f05fcf9a6032c8b706c6642a6464
Bracket- <Bracket-@users.noreply.github.com>
2014-06-11 23:12:56 -0400

    Update zlib to 1.2.8
    Now uses download_and_extract and apply_patch instead of psp-ports's
    pre-patched sources which are hard to maintain.

--- scripts/zlib.sh
+++ scripts/zlib.sh
@@ -1,3 +1,5 @@
-get_pspports zlib
-run_make -j 4
+ZLIB_VERSION=1.2.8
 
+download_and_extract http://sourceforge.net/projects/libpng/files/zlib/$ZLIB_VERSION/zlib-$ZLIB_VERSION.tar.gz zlib-$ZLIB_VERSION
+apply_patch zlib-$ZLIB_VERSION-PSP
+run_make -j 4
