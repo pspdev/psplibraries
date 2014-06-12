cbdbb5ac61908115af322378d2fa4c07d1fb2ba2
Bracket- <Bracket-@users.noreply.github.com>
2014-06-11 23:08:26 -0400

    pixman: Partially revert 9052ae6
    I forgot I was running pixman.sh directly instead
    of through libraries.sh and misunderstood the
    location of the package sources.

--- scripts/pixman.sh
+++ scripts/pixman.sh
@@ -1,6 +1,6 @@
 PIXMAN_VERSION=0.24.4
 
 download_and_extract http://cairographics.org/releases/pixman-$PIXMAN_VERSION.tar.gz pixman-$PIXMAN_VERSION
-cp -f ../patches/config.sub ./config.sub
+cp -f ../../patches/config.sub ./config.sub
 PNG_CFLAGS="-I$(psp-config --psp-prefix)/include" PNG_LIBS="-L$(psp-config --psp-prefix)/lib -lpng -lz" run_configure
 run_make -j 4
