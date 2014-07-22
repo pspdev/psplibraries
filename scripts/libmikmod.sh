d84eccff87566cbad64c31553820cf511f78a425
Diogo Autilio <diogo.autilio@titansgroup.com.br>
2014-07-22 15:53:14 -0300

    --Check for glibtoolize under Darwin

--- scripts/libmikmod.sh
+++ scripts/libmikmod.sh
@@ -1,7 +1,11 @@
 download_and_extract http://downloads.sourceforge.net/project/mikmod/outdated_versions/libmikmod/3.2.0/libmikmod-3.2.0-DONTUSE.tar.gz libmikmod-3.2.0
 apply_patch libmikmod-3.2.0-PSP
 aclocal --force
-libtoolize --force
+if [ "$(uname)" == 'Darwin' ]; then
+    glibtoolize --force
+else
+    libtoolize --force
+fi
 aclocal --force
 autoconf --force
 autoheader --force
