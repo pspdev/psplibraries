6d4dd989504fee2f35ed2c2ec5478cb19b6d4725
Diogo Autilio <diogo.autilio@titansgroup.com.br>
2014-07-22 15:03:51 -0300

    --fix SDL_gfx

--- scripts/SDL_gfx.sh
+++ scripts/SDL_gfx.sh
@@ -1,5 +1,10 @@
 test_deps_install SDL
 download_and_extract http://sourceforge.net/projects/sdlgfx/files/SDL_gfx-2.0.23.tar.gz SDL_gfx-2.0.23
 apply_patch SDL_gfx-2.0.23-PSP
-PNG_CFLAGS="-I$PSPDEV/psp/include" PNG_LIBS="-L$PSPDEV/psp/lib -lpng -lz" AR=psp-ar run_autogen_build --with-sdl-prefix=$(psp-config --psp-prefix) --disable-mmx --disable-shared
-
+aclocal --force
+aclocal --force
+autoconf --force
+automake --add-missing
+run_configure --prefix=/usr/psp --host=psp --disable-mmx --disable-shared
+run_make
+#PNG_CFLAGS="-I$PSPDEV/psp/include" PNG_LIBS="-L$PSPDEV/psp/lib -lpng -lz" AR=psp-ar run_autogen_build --with-sdl-prefix=$(psp-config --psp-prefix) --host=psp --disable-mmx --disable-shared
