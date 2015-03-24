c6e3fd594ef5f5257fd5795dec8891fe84dfd368
Carsten Teibes <dev@f4ke.de>
2015-03-25 00:20:22 +0100

    copy config.sub to add psp host definition back in

--- scripts/SDL_gfx.sh
+++ scripts/SDL_gfx.sh
@@ -2,5 +2,6 @@ test_deps_install SDL
 download_and_extract http://sourceforge.net/projects/sdlgfx/files/SDL_gfx-2.0.25.tar.gz SDL_gfx-2.0.25
 apply_patch SDL_gfx-2.0.25-PSP
 autoreconf --force --install
+cp ../../patches/config.sub ./config.sub
 run_configure --prefix=$(psp-config --psp-prefix) --host=psp --disable-mmx --disable-shared
 run_make
