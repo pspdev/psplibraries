4f2643c1f7d909504d4cfee2e7f251ce95da0f3c
Marek Vybiral <marekvybiral@email.cz>
2015-03-24 23:56:50 +0100

    removed commands which are run by autoreconf

--- scripts/SDL_gfx.sh
+++ scripts/SDL_gfx.sh
@@ -1,10 +1,7 @@
 test_deps_install SDL
 download_and_extract http://sourceforge.net/projects/sdlgfx/files/SDL_gfx-2.0.23.tar.gz SDL_gfx-2.0.23
-autoreconf --force --install
 apply_patch SDL_gfx-2.0.23-PSP
-aclocal --force
-autoconf --force
-automake --add-missing
+autoreconf --force --install
 run_configure --prefix=$(psp-config --psp-prefix) --host=psp --disable-mmx --disable-shared
 run_make
 #PNG_CFLAGS="-I$PSPDEV/psp/include" PNG_LIBS="-L$PSPDEV/psp/lib -lpng -lz" AR=psp-ar run_autogen_build --with-sdl-prefix=$(psp-config --psp-prefix) --host=psp --disable-mmx --disable-shared
