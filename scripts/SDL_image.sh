3a6bfa56d59c86b21ac49e5a25baf8e87e374812
David Guillen Fandos <david@davidgf.net>
2015-10-19 01:19:57 +0100

    Fix SDL_image 1.11+ build (webp is not shipped with psplibraries but required by default)

--- scripts/SDL_image.sh
+++ scripts/SDL_image.sh
@@ -2,5 +2,5 @@ test_deps_install SDL libpng jpeg
 
 download_and_extract http://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz SDL_image-1.2.12
 apply_patch SDL_image-1.2.12-PSP
-LDFLAGS="-lpspirkeyb" run_autogen_build --with-sdl-prefix=$(psp-config --psp-prefix)
+LDFLAGS="-lpspirkeyb" run_autogen_build --disable-webp --with-sdl-prefix=$(psp-config --psp-prefix)
 
