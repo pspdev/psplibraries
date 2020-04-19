d7cd01109d4253e3fb879c1f616c70b5f05316d6
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2020-04-19 02:55:46 -0400

    Add PerfLib

---
+++ scripts/libperf.sh
@@ -0,0 +1,5 @@
+download_and_extract https://github.com/NT-Bourgeois-Iridescence-Technologies/PerfLib/archive/master.zip PerfLib
+make
+mkdir -p $(psp-config --psp-prefix)/include/
+cp -v include/*.h $(psp-config --psp-prefix)/include/
+cp -v libPerf.a $(psp-config --psp-prefix)/lib
