18b90c2964b3dbc5f17f7c964fda80427713c791
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2020-04-19 18:24:17 -0400

    Update libme.sh

--- scripts/libme.sh
+++ scripts/libme.sh
@@ -1,7 +1,7 @@
-download_and_extract https://github.com/NT-Bourgeois-Iridescence-Technologies/MElib/archive/v1.1.tar.gz MElib
+download_and_extract https://github.com/NT-Bourgeois-Iridescence-Technologies/MElib/archive/v1.1.tar.gz MElib-1.1
 make -f MakefileLib
 mkdir -p $(psp-config --psp-prefix)/bin/MElib
 mkdir -p $(psp-config --psp-prefix)/include/MElib
 cp -v include/*.h $(psp-config --psp-prefix)/include/MElib
 cp -v bin/*.prx $(psp-config --psp-prefix)/bin/MElib
-cp -v libME.a $(psp-config --psp-prefix)/lib
+cp -v lib/libME.a $(psp-config --psp-prefix)/lib
