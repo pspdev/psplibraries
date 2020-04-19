e62cd0a06837ff156e91e89d84c446caf1b68b49
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2020-04-19 02:56:43 -0400

    Remember to add the driver PRX

--- scripts/libme.sh
+++ scripts/libme.sh
@@ -1,5 +1,7 @@
 download_and_extract https://github.com/NT-Bourgeois-Iridescence-Technologies/MElib/archive/master.zip MElib
 make
+mkdir -p $(psp-config --psp-prefix)/bin/MElib
 mkdir -p $(psp-config --psp-prefix)/include/MElib
 cp -v include/*.h $(psp-config --psp-prefix)/include/MElib
+cp -v bin/*.prx $(psp-config --psp-prefix)/bin/MElib
 cp -v libME.a $(psp-config --psp-prefix)/lib
