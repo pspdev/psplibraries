0da95e5f6cda15149846c4559c596ef34c660874
Wouter Wijsman <wwijsman@live.nl>
2020-04-26 18:17:13 +0200

    Use cmake

--- scripts/SDL2.sh
+++ scripts/SDL2.sh
@@ -1,5 +1,10 @@
+#!/bin/sh
+
 download_and_extract https://github.com/joel16/SDL2/archive/2.0.9.tar.gz SDL2-2.0.9
-make -f Makefile.psp
-mkdir -p $(psp-config --psp-prefix)/include/SDL2
-cp -v include/*.h $(psp-config --psp-prefix)/include/SDL2
-cp -v libSDL2.a $(psp-config --psp-prefix)/lib
+
+## Run cmake
+mkdir build && cd build
+psp-cmake .. || { exit 1; }
+
+## Compile and Install
+run_make -j `num_cpus`
