43e451c229b1651ad6720e46560cb9b2070ce260
Wouter Wijsman <wwijsman@live.nl>
2020-05-02 13:54:41 +0200

    Fix openal not building in pspdev-docker

--- scripts/openal.sh
+++ scripts/openal.sh
@@ -6,9 +6,7 @@ test_deps_install cmake-toolchain-script pthreads-emb
 OPENAL_VERSION=1.14
 
 download_and_extract http://kcat.strangesoft.net/openal-releases/openal-soft-$OPENAL_VERSION.tar.bz2 openal-soft-$OPENAL_VERSION
-
-## replace memory.h with string.h
-sed -i -e "s/memory\.h/string.h/" Alc/*.c Alc/backends/*.c || { exit 1; }
+apply_patch openal-$OPENAL_VERSION-PSP
 
 ## Run cmake
 psp-cmake -D CMAKE_INSTALL_PREFIX=$(psp-config --psp-prefix) -D UTILS=OFF -D EXAMPLES=OFF . || { exit 1; }
