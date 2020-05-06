3e57bf8796632cfde455c5f4e833f5d1e67558a6
Wouter Wijsman <wwijsman@live.nl>
2020-05-06 20:04:07 +0200

    Include SDL2main

--- scripts/SDL2.sh
+++ scripts/SDL2.sh
@@ -1,5 +1,7 @@
 download_and_extract https://github.com/joel16/SDL2/archive/2.0.9.tar.gz SDL2-2.0.9
 make -f Makefile.psp
+make -f Makefile.main.psp
 mkdir -p $(psp-config --psp-prefix)/include/SDL2
 cp -v include/*.h $(psp-config --psp-prefix)/include/SDL2
 cp -v libSDL2.a $(psp-config --psp-prefix)/lib
+cp -v libSDL2main.a $(psp-config --psp-prefix)/lib
