127636bdb00df290ab4bb921bc61d34ac23c5f65
Carsten Teibes <dev@f4ke.de>
2020-05-07 19:55:40 +0200

    Update libmikmod to 3.3.11.1, simplify patch

--- scripts/libmikmod.sh
+++ scripts/libmikmod.sh
@@ -1,14 +1,5 @@
-download_and_extract http://downloads.sourceforge.net/project/mikmod/outdated_versions/libmikmod/3.2.0/libmikmod-3.2.0-DONTUSE.tar.gz libmikmod-3.2.0
-apply_patch libmikmod-3.2.0-PSP
-aclocal --force
-if [ "$(uname)" == 'Darwin' ]; then
-    glibtoolize --force
-else
-    libtoolize --force
-fi
-aclocal --force
-autoconf --force
-autoheader --force
-automake --add-missing
-run_configure --disable-esd --enable-osx=no
+download_and_extract https://downloads.sourceforge.net/mikmod/libmikmod-3.3.11.1.tar.gz libmikmod-3.3.11.1
+
+apply_patch libmikmod-3.3.11.1-PSP
+run_configure --disable-alldrv --enable-psp --enable-aiff --enable-wav --enable-raw
 run_make -j `num_cpus`
