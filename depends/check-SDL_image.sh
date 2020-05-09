cea1eb122adfcbb2cfaf867513dd58fdc41c76bc
Carsten Teibes <dev@f4ke.de>
2020-05-09 18:10:14 +0200

    SDL_image: Use psp-port

--- depends/check-SDL_image.sh
+++ depends/check-SDL_image.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
- ls $(psp-config --psp-prefix)/include/SDL/SDL_image.h $(psp-config --psp-prefix)/lib/libSDL_image.a
+#!/bin/bash
 
+PREFIX=$(psp-config --psp-prefix)
+
+ls "$PREFIX"/include/SDL/SDL_image.h \
+   "$PREFIX"/lib/libSDL_image.a
--- scripts/SDL_image.sh
+++ scripts/SDL_image.sh
@@ -1,6 +1,4 @@
 test_deps_install SDL libpng jpeg
-
-download_and_extract http://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz SDL_image-1.2.12
-apply_patch SDL_image-1.2.12-PSP
-LIBPNG_CFLAGS="$CFLAGS -I$(psp-config --psp-prefix)/include/libpng15/" LDFLAGS="-lpspirkeyb" run_autogen_build --disable-webp --with-sdl-prefix=$(psp-config --psp-prefix)
-
+get_pspport SDL_image SDL_image-psp
+PKG_CONFIG_LIBDIR="$(psp-config --psp-prefix)/lib/pkgconfig" \
+  run_autogen_build --with-sdl-prefix=$(psp-config --psp-prefix)
