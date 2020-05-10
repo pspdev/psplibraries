342e8e6fe2826b7b9892a8e16e8ed8581f773441
Carsten Teibes <dev@f4ke.de>
2020-05-11 00:45:03 +0200

    Update freetype from 2.1.10 to 2.10.2

--- scripts/freetype.sh
+++ scripts/freetype.sh
@@ -1,4 +1,4 @@
 test_deps_install zlib
-get_pspport freetype2 freetype2-psp-legacy
+get_pspport freetype2 freetype2-psp
 PKG_CONFIG_LIBDIR="$(psp-config --psp-prefix)/lib/pkgconfig" \
-  run_autogen_build
+  run_autogen_build --without-bzip2 --without-png
