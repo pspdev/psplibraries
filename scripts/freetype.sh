89ca1a54eab9695543ea1acdaacda5ea46394218
Carsten Teibes <dev@f4ke.de>
2020-05-11 00:44:36 +0200

    freetype: Use psp-port

--- scripts/freetype.sh
+++ scripts/freetype.sh
@@ -1,3 +1,4 @@
-get_pspports freetype
-run_autogen_build
-
+test_deps_install zlib
+get_pspport freetype2 freetype2-psp-legacy
+PKG_CONFIG_LIBDIR="$(psp-config --psp-prefix)/lib/pkgconfig" \
+  run_autogen_build
