d21b0da072f0b00cb31baccacb5d390898ec7665
Carsten Teibes <dev@f4ke.de>
2020-05-11 16:47:12 +0200

    Fix SDL2_gfx and SDL2_mixer dependency on SDL2

--- scripts/SDL2_mixer.sh
+++ scripts/SDL2_mixer.sh
@@ -1,4 +1,4 @@
-test_deps_install SDL libmikmod
+test_deps_install SDL2 libmikmod
 get_pspport SDL_mixer SDL2_mixer-psp
 PKG_CONFIG_LIBDIR="$(psp-config --psp-prefix)/lib/pkgconfig" \
   SDL_CFLAGS="-I$(psp-config --psp-prefix)/include/SDL2" SDL_LIBS="-lSDL2" \
