1d829afc7d1adf1651d83d662c2c38742f300910
Carsten Teibes <dev@f4ke.de>
2020-05-12 19:02:51 +0200

    SDL_ttf: Force pkg-config directory

--- scripts/SDL_ttf.sh
+++ scripts/SDL_ttf.sh
@@ -1,4 +1,5 @@
 test_deps_install SDL freetype
 get_pspport SDL_ttf SDL_ttf-psp
-run_autogen_build --with-sdl-prefix=$(psp-config --psp-prefix) \
+PKG_CONFIG_LIBDIR="$(psp-config --psp-prefix)/lib/pkgconfig" \
+  run_autogen_build --with-sdl-prefix=$(psp-config --psp-prefix) \
   --with-freetype-prefix=$(psp-config --psp-prefix) --without-x
