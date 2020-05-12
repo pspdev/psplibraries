b10432eb5b36e16d8e05d89886a8d76af2fb88ae
Carsten Teibes <dev@f4ke.de>
2020-05-12 18:10:57 +0200

    Remove SDL_gfx patching This should be done in pspdev/SDL_gfx

--- scripts/SDL_gfx.sh
+++ scripts/SDL_gfx.sh
@@ -1,6 +1,4 @@
 test_deps_install SDL
 get_pspport SDL_gfx SDL_gfx-psp
-autoreconf --force --install -I $(psp-config --psp-prefix)/share/aclocal
-cp ../../patches/config.sub ./config.sub
 run_autogen_build --with-sdl-prefix=$(psp-config --psp-prefix) \
   --disable-mmx
