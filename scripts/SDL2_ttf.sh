c8c3c3f983b6d92619cad2d42fac895b12209262
Carsten Teibes <dev@f4ke.de>
2020-05-12 18:59:33 +0200

    Re-enable freetype-config script

--- scripts/SDL2_ttf.sh
+++ scripts/SDL2_ttf.sh
@@ -2,4 +2,5 @@ test_deps_install SDL2 freetype
 get_pspport SDL_ttf SDL2_ttf-psp
 PKG_CONFIG_LIBDIR="$(psp-config --psp-prefix)/lib/pkgconfig" \
   SDL_CFLAGS="-I$(psp-config --psp-prefix)/include/SDL2" SDL_LIBS="-lSDL2" \
-  run_autogen_build --without-x
+  run_autogen_build --with-freetype-prefix=$(psp-config --psp-prefix) \
+  --without-x
