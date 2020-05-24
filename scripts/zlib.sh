719bee280b82346764dcf072e5ded257f0e6325f
Wouter Wijsman <wwijsman@live.nl>
2020-05-24 13:09:06 +0200

    Remove scripts replaced by PSPBUILDs

--- scripts/zlib.sh
+++
@@ -1,5 +0,0 @@
-ZLIB_VERSION=1.2.8
-
-download_and_extract http://sourceforge.net/projects/libpng/files/zlib/$ZLIB_VERSION/zlib-$ZLIB_VERSION.tar.gz zlib-$ZLIB_VERSION
-apply_patch zlib-$ZLIB_VERSION-PSP
-run_make -j `num_cpus`
