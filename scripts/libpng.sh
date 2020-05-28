fcbfab3f58d43598ff2a70c1270b8b7a10f56292
Wouter Wijsman <wwijsman@live.nl>
2020-05-28 23:14:03 +0200

    Remove scripts for libraries which have PSPBUILDs

--- scripts/libpng.sh
+++
@@ -1,8 +0,0 @@
-test_deps_install zlib
-
-LIBPNG_VERSION=1.5.7
-
-download_and_extract http://sourceforge.net/projects/libpng/files/libpng15/older-releases/$LIBPNG_VERSION/libpng-$LIBPNG_VERSION.tar.gz libpng-$LIBPNG_VERSION
-apply_patch libpng-$LIBPNG_VERSION-PSP
-run_configure
-run_make -j `num_cpus`
