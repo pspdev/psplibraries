66348a73b79adc89fb4ce3381ad6026115dbe3b0
Wouter Wijsman <wwijsman@live.nl>
2020-05-28 23:14:03 +0200

    Remove scripts for libraries which have PSPBUILDs

--- scripts/libmikmod.sh
+++
@@ -1,5 +0,0 @@
-download_and_extract https://downloads.sourceforge.net/mikmod/libmikmod-3.3.11.1.tar.gz libmikmod-3.3.11.1
-
-apply_patch libmikmod-3.3.11.1-PSP
-run_configure --disable-alldrv --enable-psp --enable-aiff --enable-wav --enable-raw
-run_make -j `num_cpus`
