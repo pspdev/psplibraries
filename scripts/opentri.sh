d5e9d9006195e952bf1eeb52f122fbc38e87e668
Wouter Wijsman <wwijsman@live.nl>
2020-05-29 20:48:13 +0200

    Port openTRI to PSPBUILD

--- scripts/opentri.sh
+++
@@ -1,6 +0,0 @@
-test_deps_install zlib freetype
-
-wget --continue --no-check-certificate https://github.com/SamRH/openTRI/tarball/master -O openTRI.tar.gz
-rm -Rf openTRI && mkdir openTRI && tar --strip-components=1 --directory=openTRI -xvzf openTRI.tar.gz
-cd openTRI
-CP=cp MKDIR=mkdir DOXYGEN=doxygen PNG=1 FT=1 run_make -j `num_cpus`
