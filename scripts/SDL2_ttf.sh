0e0171e75d923412f294f182f9912c1843191d90
Wouter Wijsman <wwijsman@live.nl>
2020-05-30 20:45:15 +0200

    Add PSPBUILDS for SDL(2)_ttf

--- scripts/SDL2_ttf.sh
+++
@@ -1,8 +0,0 @@
-test_deps_install SDL2 freetype
-get_pspport SDL_ttf SDL2_ttf-psp
-
-# This is used when there is no pkg-config, but needs sdl2-config
-#EXTRA_FLAGS="--with-sdl-prefix=$(psp-config --psp-prefix)"
-# Force pkg-config flags, until our SDL2 ships it
-SDL_CFLAGS="-I$(psp-config --psp-prefix)/include/SDL2" SDL_LIBS="-lSDL2" \
-  run_autogen_build $EXTRA_FLAGS --without-x
