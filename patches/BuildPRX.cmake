96a9a0e686a1d892536875b1d3b050a1695423d2
Wouter Wijsman <wwijsman@live.nl>
2020-06-13 00:43:10 +0200

    Simplify BuildPRX by using a macro and unused variables

--- patches/BuildPRX.cmake
+++ patches/BuildPRX.cmake
@@ -1,12 +1,7 @@
-function(build_prx elf)
-	set(BUILD_PRX TRUE)
-	set(_PRX_FLAGS "-g3 -specs=${PSPSDK}/lib/prxspecs -Wl,-q,-T${PSPSDK}/lib/linkfile.prx")
-	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS_DEBUG} ${PRX_FLAGS}")
-	set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS_DEBUG} ${PRX_FLAGS}")
-
+macro(build_prx elf)
 	add_custom_command(
 		TARGET ${elf}
 		POST_BUILD COMMAND
 		${PRXGEN} ${elf} ${elf}.prx
 	)
-endfunction()
+endmacro()
