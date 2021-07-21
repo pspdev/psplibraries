749436d7284e51bedcffc16a6d7b6a7dd228b0e1
Wally4000 <catapultit@me.com>
2021-07-21 16:49:50 +1000

    Update AngelScript from 2.23.0 -> 2.35.0. Fixed script so it actually copies the library

--- scripts/angelscript.sh
+++ scripts/angelscript.sh
@@ -1,7 +1,7 @@
 ## test dependency
 test_deps_install cmake-toolchain-script
 
-ANGELSCRIPT_VERSION=2.23.0
+ANGELSCRIPT_VERSION=2.35.0
 
 download_and_extract http://www.angelcode.com/angelscript/sdk/files/angelscript_$ANGELSCRIPT_VERSION.zip sdk
 cd angelscript/projects/cmake || { return 1; }
@@ -12,4 +12,4 @@ psp-cmake -D CMAKE_INSTALL_PREFIX=$(psp-config --psp-prefix) . || { exit 1; }
 ## Compile and Install
 make -j `num_cpus` || { exit 1; }
 cp -v ../../include/*.h $(psp-config --psp-prefix)/include
-cp -v ../../lib/lib*.a $(psp-config --psp-prefix)/lib
+cp -v lib*.a $(psp-config --psp-prefix)/lib
