17d458320d21a4d53feba5cf98f0ce940a508f87
Wally4000 <catapultit@me.com>
2021-07-22 18:41:04 +1000

    Apply fjtrujy's branch for SDL2 script

--- scripts/SDL2.sh
+++ scripts/SDL2.sh
@@ -1,8 +1,8 @@
 test_deps_install libpspvram pspgl
-download_and_extract https://github.com/joel16/SDL2/archive/SDL-2.0.12.tar.gz SDL2-SDL-2.0.12
+git clone https://github.com/pspdev/SDL SDL2 -b psp-v2.0.14 --depth=1
+cd SDL2
 make -f Makefile.psp
-make -f Makefile.main.psp
+
 mkdir -p $(psp-config --psp-prefix)/include/SDL2
 cp -v include/*.h $(psp-config --psp-prefix)/include/SDL2
 cp -v libSDL2.a $(psp-config --psp-prefix)/lib
-cp -v libSDL2main.a $(psp-config --psp-prefix)/lib
