928e0de4d545fd2dfd9f585a153cb422970e76fd
Wally4000 <catapultit@me.com>
2021-07-22 18:51:36 +1000

    Update SDL to use the PSP Repo

--- scripts/SDL.sh
+++ scripts/SDL.sh
@@ -1,5 +1,4 @@
-download_and_extract http://www.libsdl.org/release/SDL-1.2.15.tar.gz SDL-1.2.15
-apply_patch SDL-1.2.15-PSP
-apply_patch SDL_glfuncs.h
+git clone https://github.com/pspdev/SDL/ SDL -b  psp-v.1.2.15 --depth=1
+cd SDL
 run_autogen_build --enable-pspirkeyb
 
