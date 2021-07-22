05f036efa56e121a14b39cbdeffc124eb5e045e6
Wally4000 <catapultit@me.com>
2021-07-22 22:04:04 +1000

    Roll back zlib patch for now as it seems to fail on examples.

--- scripts/zlib.sh
+++ scripts/zlib.sh
@@ -1,4 +1,6 @@
 ZLIB_VERSION=1.2.11
 
 download_and_extract http://sourceforge.net/projects/libpng/files/zlib/$ZLIB_VERSION/zlib-$ZLIB_VERSION.tar.gz zlib-$ZLIB_VERSION
-psp-cmake $* .; make -j `num_cpus`; make install
+#psp-cmake $* .; make -j `num_cpus`; make install - Does not work just yet
+apply_patch zlib-$ZLIB_VERSION-PSP
+run_make -j `num_cpus`
